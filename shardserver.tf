3f34b921b13cad77f4233ab22dfbc6df67cc90c4
kenichishibata <kenichi.shibata3123@gmail.com>
2016-12-29 17:19:38 +0900

    fixed int conv

--- shardserver.tf
+++ shardserver.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "shards" {
 	ami = "${lookup(var.amazon_amis, var.region)}"
 
-	count = "${var.count_shardsvr} * ${var.count_shardsvr_replica}"
+	count = "${var.count_shardsvr * var.count_shardsvr_replica}"
 
 	tags = {
 		Name = "${var.pre_tag}-${var.service_name}-shard0${count.index}"
